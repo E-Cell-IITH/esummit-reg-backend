a172553f5fd6952bfd0ef4a2a61559c73810c3e2
Bhaskar <bhaskarmandal369@gmail.com>
2025-01-02 23:45:50 +0530

    feat: add data model

---
+++ internal/model/data_model.go
@@ -0,0 +1,18 @@
+package model
+
+type RegistrationData struct {
+	SName    string `json:"sname"`
+	FName    string `json:"fname"`
+	POCName  string `json:"pocname"`
+	Contact  string `json:"contact"`
+	Startup  string `json:"startup"`
+	Service  string `json:"service"`
+	Email    string `json:"email"`
+	SEmail   string `json:"semail"`
+	IFocus   string `json:"ifocus"`
+	AYears   string `json:"ayears"`
+	Location string `json:"location"`
+	City     string `json:"city"`
+	About    string `json:"about"`
+	IDToken  string `json:"id_token"`
+}
